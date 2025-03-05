1d3cddaef9fcb51c1b5976221d9c25d9964e6768
callalilyleaf <che23035@byui.edu>
2025-03-04 20:53:33 -0700

    Finished W9 RESTful Web service with Springboot assignment

---
+++ web/src/test/java/edu/byui/apj/storefront/web/WebApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.byui.apj.storefront.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("200 OK");
+    }
+
+}
