9deb313875386e0f3e112c65441c6485d2dc827e
callalilyleaf <che23035@byui.edu>
2025-03-11 22:23:39 -0600

    update for W10 Accessing Data with JPA Tutorial assignment. To be finished: run 1 application among 2, rest of the procedures after 2 tutorials

---
+++ tutorial10-1/src/test/java/edu/byui/apj/storefront/tutorial101/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.byui.apj.storefront.tutorial101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
