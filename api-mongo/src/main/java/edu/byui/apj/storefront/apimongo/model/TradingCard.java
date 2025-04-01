2b002fb1a241ab05c3837eba46691e1d5e473c89
callalilyleaf <che23035@byui.edu>
2025-04-01 14:09:13 -0600

    test for github connection

--- api-mongo/src/main/java/edu/byui/apj/storefront/apimongo/model/TradingCard.java
+++ api-mongo/src/main/java/edu/byui/apj/storefront/apimongo/model/TradingCard.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+
 @Data
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
